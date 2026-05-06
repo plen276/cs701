2d9c568b6db4a2861ad997055457badcd599ba4b
Pulasthi Lenaduwa <69018605+plen276@users.noreply.github.com>
2026-05-06 16:07:40 +1200

    [LAB2]: Modify the code to take in to account for the two channels and their averages

--- lab-2/src/DpAsp.vhd
+++ lab-2/src/DpAsp.vhd
@@ -14,28 +14,42 @@ entity DpAsp is
 end entity;
 
 architecture rtl of DpAsp is
-    signal avg : signed(15 downto 0) := (others => '0');
+    signal avg_ch0 : signed(15 downto 0) := (others => '0');
+    signal avg_ch1 : signed(15 downto 0) := (others => '0');
 begin
 
     process(clock)
-        -- shift register: s0 = newest, s3 = oldest
-        variable s0, s1, s2, s3 : signed(15 downto 0) := (others => '0');
+        -- separate shift registers per channel: s0 = newest, s3 = oldest
+        variable s0_0, s1_0, s2_0, s3_0 : signed(15 downto 0) := (others => '0');
+        variable s0_1, s1_1, s2_1, s3_1 : signed(15 downto 0) := (others => '0');
         variable sum    : signed(17 downto 0);
         variable avg_v  : signed(15 downto 0);
         variable result : signed(16 downto 0);
     begin
         if rising_edge(clock) then
             if recv.data(31 downto 28) = "1000" then
-                -- shift in new sample
-                s3 := s2;
-                s2 := s1;
-                s1 := s0;
-                s0 := signed(recv.data(15 downto 0));
 
-                -- moving average of 4 samples (sum then divide by 4)
-                sum   := resize(s0, 18) + resize(s1, 18) + resize(s2, 18) + resize(s3, 18);
-                avg_v := signed(sum(17 downto 2));  -- arithmetic right shift by 2
-                avg   <= avg_v;
+                if recv.data(16) = '0' then
+                    -- channel 0
+                    s3_0 := s2_0;
+                    s2_0 := s1_0;
+                    s1_0 := s0_0;
+                    s0_0 := signed(recv.data(15 downto 0));
+
+                    sum   := resize(s0_0, 18) + resize(s1_0, 18) + resize(s2_0, 18) + resize(s3_0, 18);
+                    avg_v := signed(sum(17 downto 2));
+                    avg_ch0 <= avg_v;
+                else
+                    -- channel 1
+                    s3_1 := s2_1;
+                    s2_1 := s1_1;
+                    s1_1 := s0_1;
+                    s0_1 := signed(recv.data(15 downto 0));
+
+                    sum   := resize(s0_1, 18) + resize(s1_1, 18) + resize(s2_1, 18) + resize(s3_1, 18);
+                    avg_v := signed(sum(17 downto 2));
+                    avg_ch1 <= avg_v;
+                end if;
 
                 -- double the average, then clip |result| to 4096
                 result := resize(avg_v, 17) + resize(avg_v, 17);
